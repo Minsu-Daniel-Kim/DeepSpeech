d6082ea939c03e2b3056f495a8fecf9601fabdef
JRMeyer <joshuameyer@email.arizona.edu>
2018-11-15 06:41:57 +0300

    prettier

--- filter_train_dev_test.py
+++ filter_train_dev_test.py
@@ -26,30 +26,47 @@ output_folder = sys.argv[2]
 #ABS_PATH='/snakepit/shared/data/mozilla/CommonVoice/v2.0-alpha1.0'
 ABS_PATH='/home/josh/CV'
 
+
+
+
+####           ####
+#### CLIPS.TSV ####
+####           ####
+
+
 # First, we import the main csv file which stores all the data for all languages,
 # whether or not they've been validated (the file is called clips.tsv) 
 # clips.tsv ==  path	sentence    up_votes	down_votes   age     gender	accent	locale	bucket
+
 clips = pandas.read_csv('{}/clips.tsv'.format(ABS_PATH), sep='\t')
 print("Looking for clips.tsv here: ", '{}/clips.tsv'.format(ABS_PATH))
 # pull out data for just one language
 locale = clips[clips['locale'] == LOCALE]
-
-# format file names and add abs path
+# format file names
 locale['path'] = locale['path'].str.replace('/', '___')
 locale['path'] = locale['path'].str.replace('mp3', 'wav')
 dev_paths = locale[locale['bucket'] == 'dev'].loc[:, ['path']]
 test_paths = locale[locale['bucket'] == 'test'].loc[:, ['path']]
 train_paths = locale[locale['bucket'] == 'train'].loc[:, ['path']]
 
-# dev_paths['path'] = '{}/{}/valid/'.format(ABS_PATH, LOCALE) + dev_paths['path'].astype(str)
-# test_paths['path'] = '{}/{}/valid/'.format(ABS_PATH, LOCALE) + test_paths['path'].astype(str)
-# train_paths['path'] = '{}/{}/valid/'.format(ABS_PATH, LOCALE) + train_paths['path'].astype(str)
 
+
+####                   ####
+#### CV_LANG_VALID.TSV ####
+####                   ####
 
 # cv_LANG_valid.csv == wav_filename,wav_filesize,transcript
+
 validated_clips = pandas.read_csv('{}/{}/cv_{}_valid.csv'.format(ABS_PATH, LOCALE, LOCALE))
 validated_clips['path'] = validated_clips['wav_filename'].apply(ntpath.basename)
 validated_clips['transcript'] =  validated_clips['transcript'].str.replace(u'\xa0', ' ') # for ky only?
+
+
+
+
+####              ####
+#### EXTRACT SETS ####
+####              ####
 
 # produces a single column with a Bool for whether or not the validated clip is in dev / train / test
 dev_indices = validated_clips['path'].isin(dev_paths['path'])
@@ -63,8 +80,6 @@ print("Num validated clips to be used in DEV: ", validated_clips[dev_indices]['w
 print("Num validated clips to be used in TEST: ", validated_clips[test_indices]['wav_filename'].count())
 print("Num validated clips to be used in TRAIN: ", validated_clips[train_indices]['wav_filename'].count())
 print("###############################################")
-
-
 
 validated_clips[dev_indices].to_csv(os.path.join(output_folder, 'cv_{}_valid_dev.csv'.format(LOCALE)), index=False)
 validated_clips[test_indices].to_csv(os.path.join(output_folder, 'cv_{}_valid_test.csv'.format(LOCALE)), index=False)
